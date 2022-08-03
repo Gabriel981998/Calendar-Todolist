a7140a36cf10c550d6c08a79663fa54415aed6f6
Gabe Ntambo <gabentambo1998@gmail.com>
2022-08-03 13:24:11 +0300

    Gabe first Calendar Todolist commit

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ let nav = 0;
 let clicked = null;
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
+
+
 const calendar = document.getElementById('calendar');
 const newEventModal = document.getElementById('newEventModal');
 const deleteEventModal = document.getElementById('deleteEventModal');
+
+
 const backDrop = document.getElementById('modalBackDrop');
 const eventTitleInput = document.getElementById('eventTitleInput');
+const print= document.getElementById('print');
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 function openModal(date) {
   clicked = date;
+ 
 
   const eventForDay = events.find(e => e.date === clicked);
 
@@ -51,9 +57,9 @@ function load() {
 
   document.getElementById('monthDisplay').innerText = 
     `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
-
+var array=JSON.stringify(events)
   calendar.innerHTML = '';
-
+  
   for(let i = 1; i <= paddingDays + daysInMonth; i++) {
     const daySquare = document.createElement('div');
     daySquare.classList.add('day');
@@ -86,6 +92,10 @@ function load() {
   }
 }
 
+
+
+
+
 function closeModal() {
   eventTitleInput.classList.remove('error');
   newEventModal.style.display = 'none';
@@ -93,6 +103,7 @@ function closeModal() {
   backDrop.style.display = 'none';
   eventTitleInput.value = '';
   clicked = null;
+  
   load();
 }
 
@@ -107,6 +118,7 @@ function saveEvent() {
 
     localStorage.setItem('events', JSON.stringify(events));
     closeModal();
+    
   } else {
     eventTitleInput.classList.add('error');
   }
@@ -117,6 +129,7 @@ function deleteEvent() {
   localStorage.setItem('events', JSON.stringify(events));
   closeModal();
 }
+
 
 function initButtons() {
   document.getElementById('nextButton').addEventListener('click', () => {
@@ -135,5 +148,95 @@ function initButtons() {
   document.getElementById('closeButton').addEventListener('click', closeModal);
 }
 
+
+
+
+
 initButtons();
 load();
+
+
+/*
+(print to screen!!)
+var array=JSON.stringify(events);
+print.innerHTML=array;
+
+*/
+
+/*
+
+
+(ALERT Message!!!!)
+  for(i =0;i<events.length;i++){
+    alert(events[i].title+" Will be on "+events[i].date);
+
+  }
+
+*/
+
+events ;
+ 
+
+
+
+buildTable(events)
+
+
+
+$('th').on('click', function(){
+   var column = $(this).data('colname')
+   var order = $(this).data('order')
+   var text = $(this).html()
+   text = text.substring(0, text.length - 1);
+   
+   
+   
+   if (order == 'desc'){
+    events   = events .sort((a, b) => a[column] > b[column] ? 1 : -1)
+      $(this).data("order","asc");
+      text += '&#9660'
+   }else{
+    events  = events .sort((a, b) => a[column] < b[column] ? 1 : -1)
+      $(this).data("order","desc");
+      text += '&#9650'
+   }
+
+  $(this).html(text)
+  buildTable(events )
+  })
+
+
+ 
+
+  
+function buildTable(data){
+  var table = document.getElementById('myTable')
+  table.innerHTML = ''
+  for (var i = 0; i < data.length; i++){
+      var coltitle = `title-${i}`
+      var coldate = `date-${i}`
+     
+
+      var row = `<tr>
+                      <td>${data[i].title}</td>
+                      <td>${data[i].date}</td>
+                     
+                  
+                 </tr>`
+      table.innerHTML += row
+  }
+}
+
+function show(){
+document.getElementById('table-striped').style.display="block";
+}
+
+
+
+
+
+
+
+
+
+
